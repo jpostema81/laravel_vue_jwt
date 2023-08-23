28f7117ab9c90399874ecf2b4d1d8fa03b4ce507
Jeroen Postema <jeroen@script.nl>
2023-08-23 15:47:38 +0200

    add project files

---
+++ resources/js/views/LoginView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import axios from "axios";
+const login = () => {
+    axios.post("api/login", { email: "test@test.com", password: "" });
+};
+</script>
+
+<template>
+    <div class="about">
+        <h1>This is the login page</h1>
+
+        <button type="button" @click="login()">Login</button>
+    </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
